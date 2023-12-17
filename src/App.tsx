398b7dccda0f34030d45dc4ccd04040158ebd55a
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-12-17 16:49:44 -0500

    start project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center align-items-center p-5'>
+      <h1 className='text-4xl center'>Store with React</h1>
+    </div>
+  )
+}
+
+export default App
